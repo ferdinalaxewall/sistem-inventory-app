e8df38e9b7561b97295e17d800dbe8ece6ad1132
ferdinalaxewall <didan699@gmail.com>
2024-05-27 00:49:56 +0700

    Enhancement/Footer: Optimize Copywriting in Footer Component

--- resources/views/dashboard/components/footer.blade.php
+++ resources/views/dashboard/components/footer.blade.php
@@ -2,11 +2,11 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
       <div class="mb-2 mb-md-0">
-        Copyright © {{ now()->year }}, All Right Reserved.
+        Copyright © {{ now()->year }} {{ config('app.name') }}
       </div>
 
       <div class="">
-        <strong>Kelompok 43 - {{ config('app.name') }}</strong>
+        <strong>Kelompok 43 - Universitas Bina Sarana Informatika</strong>
       </div>
     </div>
 </footer>
