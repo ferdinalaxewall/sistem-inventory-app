01fcf777742172874db16223f9e0e3be638c47a0
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-05-27 20:07:44 -0700

    -

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -140,7 +140,6 @@ class AuthController extends Controller
             ]);
     
             $mail = Mail::to($user->email)->send(new VerificationCodeMail($verificationCode, $user->name, $user->uuid));
-            dd($mail);
             
             return ObjectResponse::success(__('auth.verification_code'), 201);
         } catch (\Throwable $th) {
