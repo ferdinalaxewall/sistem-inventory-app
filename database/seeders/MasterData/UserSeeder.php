ca6704f810440a68e82a5a0b9ff02093594ce560
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-05-27 22:26:40 -0700

    -

--- database/seeders/MasterData/UserSeeder.php
+++ database/seeders/MasterData/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         \App\Models\User::create([
             'name' => 'Super Admin',
             'code' => (new User)->generateUniqueCode(USER::ADMIN_PREFIX_CODE),
-            'email' => 'admin@stockflow.site',
+            'email' => 'admin@stockflow.fun',
             'phone' => '081122334455',
             'address' => 'Bogor, Indonesia',
             'email_verified_at' => now(),
